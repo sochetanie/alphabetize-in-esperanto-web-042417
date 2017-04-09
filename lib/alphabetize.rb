cb2cac80adae48afc5507e89ada3af5b41a8f1ed
Alla Gorik <allagorik2901@gmail.com>
2017-04-09 18:23:53 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esparento_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |x|
+    x.split('').map do |letter|
+      esparento_alphabet.index(letter)
+    end
+  end
 end
